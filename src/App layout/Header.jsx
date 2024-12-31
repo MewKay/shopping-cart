d9bce2d318101c1eb179ad561b8900e469020f97
MewKay <miorakanto25z@gmail.com>
2024-12-31 12:35:23 +0300

    Style Header
    Styled navigation links depending on the current path
    Changed quantity displayed on Card to limit its number to 99

--- src/App layout/Header.jsx
+++ src/App layout/Header.jsx
@@ -1,14 +1,45 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = ({ totalQuantity = 0, handleShowCartModal }) => {
+  const isHome = useMatch("/");
+  const isCart = useMatch("/store/cart");
+  const isStore = useMatch("/store/*") && !isCart;
+  const shownQuantity = totalQuantity > 99 ? "+99" : totalQuantity;
+
   return (
-    <header>
-      <h1>My Shop App</h1>
-      <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"store"}>Store</Link>
-        <button onClick={handleShowCartModal}>Cart({totalQuantity})</button>
+    <header className={styles["container"]}>
+      <h1 className={styles["brandname"]}>My Shop App</h1>
+      <nav className={styles["navigation"]}>
+        <Link
+          to={"/"}
+          className={`
+            ${styles["nav-item"]}
+            ${isHome ? styles["selected"] : ""}
+            `}
+        >
+          Home
+        </Link>
+        <Link
+          to={"store"}
+          className={`
+            ${styles["nav-item"]}
+            ${isStore ? styles["selected"] : ""}
+            `}
+        >
+          Store
+        </Link>
+        <button
+          className={`
+            ${styles["nav-item"]}
+            ${styles["cart-open-button"]}
+            ${isCart ? styles["selected"] : ""}
+            `}
+          onClick={handleShowCartModal}
+        >
+          Cart({shownQuantity})
+        </button>
       </nav>
     </header>
   );
