71d03fa3f4c3d35c0981c88240d8b2b7ecbd86f5
MewKay <miorakanto25z@gmail.com>
2025-01-06 13:25:05 +0300

    Style Home page
    Changed app #root element to span the entire screen.

--- src/Home page/Home.jsx
+++ src/Home page/Home.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 
 const Home = function createHomeComponent() {
   return (
-    <>
-      <h2>Welcome to our store!</h2>
-      <p>
+    <main className={styles["main-container"]}>
+      <h2 className={styles["hero-head"]}>Welcome to our store!</h2>
+      <p className={styles["hero-intro"]}>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus autem
         itaque corporis vitae id vero quaerat possimus nisi neque alias?
       </p>
-      <Link to={"store"}>Go to store</Link>
-    </>
+      <Link to={"store"} className={styles["store-link"]}>
+        Go to store
+      </Link>
+    </main>
   );
 };
 
