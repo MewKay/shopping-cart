29a51799e7d566bc24acaba44db721f9874d4ad5
MewKay <miorakanto25z@gmail.com>
2025-01-06 16:46:33 +0300

    Style Store page

--- src/Store page/Store.jsx
+++ src/Store page/Store.jsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
+import styles from "./Store.module.css";
 
 const Store = function createStoreComponent() {
   return (
-    <div>
-      <h2>View Our Products</h2>
-      <div>
-        <Link to={"men's clothing"}>Men&apos;s Clothing</Link>
-        <Link to={"women's clothing"}>Women&apos;s Clothing</Link>
-        <Link to={"electronics"}>Electronics</Link>
-        <Link to={"jewelery"}>Jewelery</Link>
+    <main className={styles["container"]}>
+      <div className={styles["links-container"]}>
+        <h2 className={styles["heading"]}>View Our Products</h2>
+        <div className={styles["category-container"]}>
+          <Link to={"men's clothing"} className={styles["category-link"]}>
+            Men&apos;s Clothing
+          </Link>
+          <Link to={"women's clothing"} className={styles["category-link"]}>
+            Women&apos;s Clothing
+          </Link>
+          <Link to={"electronics"} className={styles["category-link"]}>
+            Electronics
+          </Link>
+          <Link to={"jewelery"} className={styles["category-link"]}>
+            Jewelery
+          </Link>
+        </div>
+        <Link to={"all"} className={styles["all-link"]}>
+          View All
+        </Link>
       </div>
-      <Link to={"all"}>View All</Link>
-    </div>
+    </main>
   );
 };
 
